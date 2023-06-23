d27f15e1e0efc0ad3f412bcf2931d920f65935a3
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-23 01:58:00 -0400

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const delay = document.getElementById("delay");
 const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-//your code here
+//your code hereasync function showMessage() {
+            const textInput = document.getElementById('text');
+            const delayInput = document.getElementById('delay');
+            const outputDiv = document.getElementById('output');
+
+            const message = textInput.value;
+            const delay = parseInt(delayInput.value);
+
+            outputDiv.innerText = 'Loading...';
+
+            await delayFunction(delay);
+            outputDiv.innerText = message;
+        }
+
+        function delayFunction(delay) {
+            return new Promise(resolve => {
+                setTimeout(resolve, delay);
+            });
+        }
+
+        const button = document.getElementById('btn');
+        button.addEventListener('click', showMessage);
 async function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
